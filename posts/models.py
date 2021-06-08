1bbd56413da5ddef2bf0485c67d5e9f7d822cfb7
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2021-06-08 14:28:46 +0600

    Model Created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import Profiles
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    bio = models.TextField(blank=True)
+    liked = models.ManyToManyField(User, blank=True)
+    author = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    crated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profiles(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(default='avatar.png', upload_to='avatars')
+    updated = models.DateTimeField(auto_now=True)
+    crated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Profile of the user {self.user.username}"
