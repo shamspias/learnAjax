a1a18eb6f65e67ef7f558ffbfdfacb8812918a52
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2021-06-14 13:05:40 +0600

    Start basic Ajax DOM

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -8,3 +9,7 @@ from .models import Post
 def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs': qs})
+
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'Hello world'})
