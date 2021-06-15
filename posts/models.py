681f844ed22c11eb74035934d6e0aaa87a106465
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2021-06-15 16:32:54 +0600

    using css

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from profiles.models import Profiles
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    bio = models.TextField(blank=True)
+    body = models.TextField(blank=True)
     liked = models.ManyToManyField(User, blank=True)
     author = models.ForeignKey(Profiles, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,18 +1,42 @@
 console.log('hello my post')
 
 const helloWorldBox = document.getElementById('hello-world')
-
-helloWorldBox.innerHTML = 'Hello <b> World'
+const postBox = document.getElementById('posts-box')
 
 
 $.ajax({
     type: 'GET',
-    url:'hello-world',
-    success: function (response){
+    url: '/hello-world/',
+    success: function (response) {
         console.log('success', response.text)
         helloWorldBox.textContent = response.text
     },
-    error: function (error){
+    error: function (error) {
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function (response) {
+        console.log('success', response)
+        const data = response.data
+        data.forEach(el => {
+            postBox.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                  
+                  <div class="card-body">
+                    <h5 class="card-title">${el.title}</h5>
+                    <p class="card-text">${el.body}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            `
+
+        });
+    },
+    error: function (error) {
         console.log('error', error)
     }
 })
